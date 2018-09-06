f14cb01c1ecef908c62e59839e657b0d983ffad0
dani832m <31951102+dani832m@users.noreply.github.com>
2018-09-06 15:46:03 +0200

    Kast terningen vol. 1.1

--- script.js
+++ script.js
@@ -2,4 +2,16 @@
 function kastTerning() {
     var randomTal = Math.floor((Math.random() * 6) + 1);
     document.getElementById("resultat").innerHTML = randomTal;
+
+    if(randomTal == "6") {
+        document.getElementById("vurdering").innerHTML = "Du slog en sekser! Du er kongen!"
+    }
+    else {
+        document.getElementById("vurdering").innerHTML = "Du slog IKKE en sekser!"
+    }
+}
+
+function tidligereSlag() {
+    var forrigeSlag = document.getElementById("resultat").innerHTML;
+    document.getElementById("tidligereSlag").innerHTML += ", " + forrigeSlag;
 }
